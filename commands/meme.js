d3a1719e3850ded91185c6363f3723cd20bee574
FastEddyOne <89505020+FastEddyOne@users.noreply.github.com>
2023-10-23 01:27:45 -0600

    Refactor meme to use different api

--- commands/meme.js
+++ commands/meme.js
@@ -1,25 +1,31 @@
-const { MessageEmbed } = require("discord.js");
+const { CommandInteraction } = require("discord.js");
 const axios = require("axios");
+require('dotenv').config(); // Load environment variables from .env file
 
 module.exports = {
     name: "meme",
     description: "Get a random meme",
     async execute(interaction) {
         try {
-            const response = await axios.get("https://www.reddit.com/r/memes/random/.json");
-            const meme = response.data[0].data.children[0].data;
+            // Set up the headers for the RapidAPI request using the environment variable
+            const headers = {
+                "X-RapidAPI-Host": "humor-jokes-and-memes.p.rapidapi.com",
+                "X-RapidAPI-Key": process.env.RAPIDAPI_KEY, // Use the environment variable
+            };
 
-            if (meme.post_hint !== "image") {
-                return interaction.editReply("The fetched meme is not an image. Please try again.");
+            // Make the request to the RapidAPI endpoint
+            const response = await axios.get("https://humor-jokes-and-memes.p.rapidapi.com/memes/random", { headers });
+
+            const meme = response.data;
+
+            // Modify this condition based on the response format
+            if (!meme || !meme.url) {
+                return interaction.editReply("No meme found. Please try again.");
             }
 
-            const embed = new MessageEmbed()
-                .setTitle(meme.title)
-                .setImage(meme.url)
-                .setColor("#0099ff")
-                .setFooter(`👍 ${meme.ups} | 💬 ${meme.num_comments}`);
+            const messageContent = `**${meme.title}**\n${meme.url}`;
 
-            await interaction.editReply({ embeds: [embed] });
+            await interaction.editReply(messageContent);
         } catch (error) {
             console.error("Error fetching meme:", error);
             await interaction.editReply({ content: "An error occurred while fetching a meme. Please try again later.", ephemeral: true });
