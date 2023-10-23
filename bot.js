701792cd101751e5225f989c3d1150bcdeb24b58
FastEddyOne <89505020+FastEddyOne@users.noreply.github.com>
2023-10-23 01:36:10 -0600

    Add cooldown handling

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const fs = require("fs");
-const { Client, GatewayIntentBits, Collection, REST, Routes } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  REST,
+  Routes,
+} = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -25,38 +31,39 @@ async function registerCommands() {
     commandsToRegister.push({
       name: command.name,
       description: command.description,
-      options: command.options || []
+      options: command.options || [],
     });
 
     // Log the command name
     console.log(command.name);
-    
+
     // This line adds the command to the client's command collection
     client.commands.set(command.name, command);
   }
 
-  const rest = new REST({ version: '10' }).setToken(TOKEN);
+  const rest = new REST({ version: "10" }).setToken(TOKEN);
 
   // Delete all existing commands
-  const existingCommands = await rest.get(Routes.applicationCommands(client.user.id));
+  const existingCommands = await rest.get(
+    Routes.applicationCommands(client.user.id)
+  );
   for (const command of existingCommands) {
-      await rest.delete(Routes.applicationCommand(client.user.id, command.id));
+    await rest.delete(Routes.applicationCommand(client.user.id, command.id));
   }
 
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log("Started refreshing application (/) commands.");
 
     // Register commands globally
-    await rest.put(
-      Routes.applicationCommands(client.user.id),
-      { body: client.commands.map(command => ({
+    await rest.put(Routes.applicationCommands(client.user.id), {
+      body: client.commands.map((command) => ({
         name: command.name,
         description: command.description,
-        options: command.options || []
-      })) },
-    );
+        options: command.options || [],
+      })),
+    });
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
     console.error(error);
   }
@@ -68,28 +75,61 @@ client.once("ready", () => {
 });
 
 client.on("interactionCreate", async (interaction) => {
-  console.log('Received interaction:', interaction.commandName); // Add this
   if (!interaction.isCommand()) return;
-
-  await interaction.deferReply();
 
   const command = client.commands.get(interaction.commandName);
   if (!command) {
-    console.log('Command not found in collection:', interaction.commandName); // Add this
+    console.log("Command not found in collection:", interaction.commandName);
     return;
   }
 
+  // Cooldown handling
+  if (!client.cooldowns.has(command.name)) {
+    client.cooldowns.set(command.name, new Collection());
+  }
+  const now = Date.now();
+  const timestamps = client.cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+  if (timestamps.has(interaction.user.id)) {
+    const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return interaction.reply(
+        `Please wait ${timeLeft.toFixed(
+          1
+        )} more second(s) before reusing the \`${command.name}\` command.`
+      );
+    }
+  }
+  timestamps.set(interaction.user.id, now);
+  setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+  // Role restriction handling
+  if (command.roles) {
+    const memberRoles = interaction.member.roles.cache.map((role) => role.name);
+    if (!command.roles.some((role) => memberRoles.includes(role))) {
+      return interaction.reply(
+        "You do not have the required role to use this command."
+      );
+    }
+  }
+
+  // Defer the reply, then execute the command
+  await interaction.deferReply();
+
   try {
-    console.log('Attempting to execute command:', command.name); // Add this
     await command.execute(interaction);
   } catch (error) {
-    console.error(`Error executing command ${interaction.commandName} for user ${interaction.user.tag} in guild ${interaction.guild.name}:`, error);
-    await interaction.editReply({ content: 'There was an error executing the command!', ephemeral: true });
+    console.error(
+      `Error executing command ${interaction.commandName} for user ${interaction.user.tag} in guild ${interaction.guild.name}:`,
+      error
+    );
+    await interaction.editReply({
+      content: "There was an error executing the command!",
+      ephemeral: true,
+    });
   }
 });
-
-
-// Note: Cooldowns and role restrictions can be handled in the interactionCreate similar to the given code.
 
 // Start Bot
 client.login(TOKEN);
